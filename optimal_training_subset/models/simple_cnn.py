508203c5f7daf029311565ef2b912d4b544be9ba
Matthev00 <mateuszostaszewskififa@gmail.com>
2024-12-11 10:14:01 +0100

    Fix convolutional layer assignment in SimpleCNN model

--- optimal_training_subset/models/simple_cnn.py
+++ optimal_training_subset/models/simple_cnn.py
@@ -7,7 +7,7 @@ class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
-        self.conv1 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.fc1 = nn.Linear(32 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 10)
 
