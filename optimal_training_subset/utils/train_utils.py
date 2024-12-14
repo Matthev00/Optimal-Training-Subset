123ece14859366f3d944df716f6ff6d02b84d6a2
misadowsk <michalsad.protondynamic@gmail.com>
2024-12-14 14:19:06 +0100

    fix: deleted unused variables and changed balanced_accuracy to use from sklearn

--- optimal_training_subset/utils/train_utils.py
+++ optimal_training_subset/utils/train_utils.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader
+from sklearn.metrics import balanced_accuracy_score
 
 
 def train_model(
@@ -27,40 +28,14 @@ def train_model(
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    model.train()
     for epoch in tqdm(range(num_epochs)):
-        model.train()
-        train_loss = 0
         for images, labels in train_loader:
-
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            train_loss += loss.item()
-
-
-def calculate_balanced_accuracy(class_correct: list[int], class_total: list[int]) -> float:
-    """
-    Calculates the Balanced Accuracy metric based on class-level accuracies.
-
-    Args:
-        class_correct (list[int]): A list of correctly classified samples per class.
-        class_total (list[int]): A list of total samples per class.
-
-    Returns:
-        float: The Balanced Accuracy, or 0 if no samples are available for any class.
-    """
-    assert len(class_correct) == len(class_total), "class_correct and class_total must have the same length."
-    
-    accuracies = [
-        class_correct[i] / class_total[i] for i in range(len(class_total)) if class_total[i] > 0
-    ]
-
-    if len(accuracies) == 0:
-        return 0.0
-
-    return sum(accuracies) / len(accuracies)
 
 
 def loss_fn(balanced_accuracy: float, alpha: float, beta: float, S: int, D: int) -> float:
@@ -86,8 +61,9 @@ def validate_model(
     alpha: float = 1.0,
     beta: float = 0.5,
     S: int = 10,
-    D: int = 10,
-) -> tuple[float]:
+    D: int = 1000,
+    device: torch.device = torch.device("cuda"),
+) -> tuple[float, float]:
     """
     Validates the model on the validation dataset and computes the loss based on Balanced Accuracy.
 
@@ -103,24 +79,21 @@ def validate_model(
     Returns:
         Tuple[float, float]: Loss and Balanced Accuracy.
     """
-
     model.eval()
+    model.to(device)
 
-    num_classes = len(val_loader.dataset.targets.unique())
-
-    class_correct = [0] * num_classes
-    class_total = [0] * num_classes
+    all_labels = []
+    all_predictions = []
 
     with torch.inference_mode():
         for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
             output = model(images)
             _, predicted = torch.max(output.data, 1)
-            for i in range(len(labels)):
-                label = labels[i]
-                class_correct[label] += (predicted[i] == label).item()
-                class_total[label] += 1
+            all_labels.extend(labels.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
 
-    balanced_accuracy = calculate_balanced_accuracy(class_correct, class_total)
+    balanced_accuracy = balanced_accuracy_score(all_labels, all_predictions)
     loss = loss_fn(balanced_accuracy, alpha, beta, S, D)
 
     return loss, balanced_accuracy
